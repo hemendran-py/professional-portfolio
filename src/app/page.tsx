42b129c2fd7a1b75c498868d76ae0c9145a63886
hemendran-py <hemendrankumar@gmail.com>
2025-06-15 04:43:21 +0530

    uploading

---
+++ src/app/page.tsx
@@ -0,0 +1,195 @@
+import { HackathonCard } from "@/components/hackathon-card";
+import BlurFade from "@/components/magicui/blur-fade";
+import BlurFadeText from "@/components/magicui/blur-fade-text";
+import { ProjectCard } from "@/components/project-card";
+import { ResumeCard } from "@/components/resume-card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { DATA } from "@/data/resume";
+import Link from "next/link";
+import Markdown from "react-markdown";
+import { BentoCard, BentoGrid } from "@/components/magicui/bento-grid";
+import { TypingAnimation } from "@/components/magicui/typing-animation";
+import { BorderBeam } from "@/components/magicui/border-beam";
+import { cn } from "@/lib/utils";
+import { InteractiveGridPattern } from "@/components/magicui/interactive-grid-pattern";
+const BLUR_FADE_DELAY = 0.04;
+
+export default function Page() {
+  return (
+    <main className="flex flex-col min-h-[100dvh] space-y-7">
+      <section id="hero">
+        <BlurFade
+          delay={BLUR_FADE_DELAY}
+          className="overflow-hidden rounded-[15px] hero"
+        >
+          <div className="mx-auto w-full max-w-6xl space-y-8  p-4 ">
+            <div className="gap-2 flex items-start">
+              <div className="flex-col flex flex-1 space-y-1.5">
+                <BlurFadeText
+                  delay={BLUR_FADE_DELAY}
+                  className="text-xl font-bold tracking-tighter sm:text-4xl xl:text-4xl/none"
+                  yOffset={8}
+                  text={`${DATA.name} `}
+                />
+                <BlurFade delay={BLUR_FADE_DELAY}>
+                  <TypingAnimation className="max-w-[600px] md:text-lg text-sm">
+                    {DATA.description}
+                  </TypingAnimation>
+                </BlurFade>
+              </div>
+              <BlurFade delay={BLUR_FADE_DELAY}>
+                <Avatar className="size-28 border">
+                  <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
+                  <AvatarFallback>{DATA.initials}</AvatarFallback>
+                </Avatar>
+              </BlurFade>
+            </div>
+          </div>
+        </BlurFade>
+      </section>
+      <section id="about">
+        <BlurFade delay={BLUR_FADE_DELAY * 3}>
+          <h2 className="text-xl font-bold">About</h2>
+        </BlurFade>
+        <BlurFade delay={BLUR_FADE_DELAY * 4}>
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm  text-muted-foreground dark:prose-invert">
+            {DATA.summary}
+          </Markdown>
+        </BlurFade>
+      </section>
+
+      <section>
+        <BlurFade delay={BLUR_FADE_DELAY * 4.5}>
+          <div className="flex flex-wrap gap-4 mt-4">
+            {Object.entries(DATA.contact.social).map(([key, platform]) => {
+              if (!platform.url || !platform.icon) return null;
+
+              return (
+                <Link
+                  key={key}
+                  href={platform.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="flex items-center gap-2 text-sm font-medium text-muted-foreground hover:text-foreground transition"
+                >
+                  <platform.icon className="w-5 h-5" />
+                </Link>
+              );
+            })}
+          </div>
+        </BlurFade>
+      </section>
+
+      <section id="skills">
+        <div className="flex min-h-0 flex-col gap-y-3">
+          <BlurFade delay={BLUR_FADE_DELAY * 9}>
+            <h2 className="text-xl font-bold">Skills</h2>
+          </BlurFade>
+          <div className="flex flex-wrap gap-1">
+            {DATA.skills.map((skill, id) => (
+              <BlurFade key={skill} delay={BLUR_FADE_DELAY * 10 + id * 0.05}>
+                <Badge key={skill}>{skill}</Badge>
+              </BlurFade>
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section id="work">
+        <div className="flex min-h-0 gap flex-col gap-y-6">
+          <BlurFade delay={BLUR_FADE_DELAY * 5}>
+            <h2 className="text-xl font-bold">Work Experience</h2>
+          </BlurFade>
+          {DATA.work.map((work, id) => (
+            <BlurFade
+              key={work.company}
+              delay={BLUR_FADE_DELAY * 6 + id * 0.05}
+            >
+              <ResumeCard
+                key={work.company}
+                logoUrl={work.logoUrl}
+                altText={work.company}
+                title={work.company}
+                subtitle={work.title}
+                href={work.href}
+                badges={work.badges}
+                period={`${work.start} - ${work.end ?? "Present"}`}
+                description={work.description}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section id="education">
+        <div className="flex min-h-0 flex-col gap-y-6">
+          <BlurFade delay={BLUR_FADE_DELAY * 7}>
+            <h2 className="text-xl font-bold">Education</h2>
+          </BlurFade>
+          {DATA.education.map((education, id) => (
+            <BlurFade
+              key={education.school}
+              delay={BLUR_FADE_DELAY * 8 + id * 0.05}
+            >
+              <ResumeCard
+                key={education.school}
+                href={education.href}
+                logoUrl={education.logoUrl}
+                altText={education.school}
+                title={education.school}
+                subtitle={education.degree}
+                period={`${education.start} - ${education.end}`}
+              />
+            </BlurFade>
+          ))}
+        </div>
+      </section>
+      <section className="py-10 max-w-6xl mx-auto px-4">
+        <h2 className="text-3xl font-bold mb-8 text-center">🚀 My Projects</h2>
+
+        <BentoGrid>
+          {DATA.projects.map((project, index) => (
+            <BlurFade key={index} delay={BLUR_FADE_DELAY * 9 + index * 0.05}>
+              <BentoCard
+                name={project.title}
+                description={project.description}
+                href={project.href}
+                cta="Visit Project"
+                className=""
+                background={<div />} // leave empty
+                Icon={() => null} // skip icon
+                technologies={project.technologies}
+              />
+            </BlurFade>
+          ))}
+        </BentoGrid>
+      </section>
+
+      <section id="contact">
+        <div className="grid items-center justify-center gap-4 px-4 text-center md:px-6 w-full py-12">
+          <BlurFade delay={BLUR_FADE_DELAY * 16}>
+            <div className="space-y-3">
+              <div className="inline-block rounded-lg bg-foreground text-background px-3 py-1 text-sm">
+                Contact
+              </div>
+              <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                Get in Touch
+              </h2>
+              <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
+                Want to chat? Just shoot me a dm{" "}
+                <Link
+                  href={DATA.contact.social.instagram.url}
+                  className="text-blue-500 hover:underline"
+                >
+                  with a direct question on twitter
+                </Link>{" "}
+                and I&apos;ll respond whenever I can. I will ignore all
+                soliciting.
+              </p>
+            </div>
+          </BlurFade>
+        </div>
+      </section>
+    </main>
+  );
+}
